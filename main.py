0f3b9e9541cc82451fa438a1334290dcea28b2a3
Gabriel Silva Nobrega <gabrieltkdnobrega63@gmail.com>
2023-11-06 18:38:01 -0300

    fix: host & port

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ app.include_router(simulacra.router)
 if __name__ == '__main__':
     run(app=app, 
         access_log=False, 
+        host='0.0.0.0',
+        port=8080,
         # reload=True,      # dev function
         # reload_delay=5,   # dev function
         # workers=1         # dev function
