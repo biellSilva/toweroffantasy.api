a054ad00eba0a11412a729f45043acaef74bbeca
Gabriel Silva Nobrega <gabrieltkdnobrega63@gmail.com>
2023-11-06 21:00:55 -0300

    feat: simulacra enums

--- api/enums.py
+++ api/enums.py
@@ -1,6 +1,15 @@
 
-from enum import Enum
+from enum import Enum, StrEnum
+from os import listdir
+
 
 class Lang(Enum):
     en = 'en-US'
     pt = 'pt-BR'
+
+
+__simulacras_names: list[str] = []
+for file in listdir('src/data/en-US/simulacra'):
+    __simulacras_names.append(file.removesuffix('.json'))
+
+SIMULACRAS = StrEnum('SIMULACRAS', __simulacras_names)
