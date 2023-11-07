2b0e1a5186330ae804c971706f395443bb064ef2
Gabriel Silva Nobrega <gabrieltkdnobrega63@gmail.com>
2023-11-07 15:42:26 -0300

    doc: matrice route documentation

--- api/core/routes/matrices.py
+++ api/core/routes/matrices.py
@@ -1,24 +1,43 @@
 
-
 from fastapi import APIRouter
-from api.core.exceptions import ItemNotFound
 
 from api.enums import MATRICES, LANGS
-from api.infra.repository import MatriceRepo
-from api.infra.entitys import EntityBase
+
+from api.core.exceptions import ItemNotFound
 from api.core.response import PrettyJsonResponse
+
+from api.infra.repository import MatriceRepo
+from api.infra.entitys import EntityBase, Matrice
+
 
 
 router = APIRouter(prefix='/matrices', tags=['matrices'])
 
 MATRICE_REPO = MatriceRepo()
 
-@router.get('/{id}', response_model_exclude_none=True)
+@router.get('/{id}', response_model=Matrice)
 async def get_matrice(id: MATRICES, lang: LANGS = LANGS('en')):
-    ''' Get matrice based on ID '''
+    '''
+    RETURNS \n
+        Matrice
+    '''
     
     if matrice := await MATRICE_REPO.get(EntityBase(id=id), lang):
-        return PrettyJsonResponse(matrice.model_dump())
+        return PrettyJsonResponse(matrice.model_dump(exclude_none=True))
 
     else:
         raise ItemNotFound(headers={'error': f'{id} not found in {lang}'})
+
+@router.get('', response_model=dict[str, Matrice])
+async def get_all_matrices(lang: LANGS = LANGS('en')):
+    '''
+    RETURNS \n
+        Dict[Matrice.id: Matrice] 
+    '''
+    
+    if matrices := await MATRICE_REPO.get_all(lang):
+        return PrettyJsonResponse({matrice.id: matrice.model_dump(exclude_none=True) 
+                                   for matrice in matrices})
+    
+    else:
+        raise ItemNotFound(headers={'error': f'{lang} not found'})
