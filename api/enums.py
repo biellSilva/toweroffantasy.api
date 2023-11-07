10f8c9b9105d53273446bc43f6bdf38003b180c1
Gabriel Silva Nobrega <gabrieltkdnobrega63@gmail.com>
2023-11-07 00:06:10 -0300

    fix: sorted enum

--- api/enums.py
+++ api/enums.py
@@ -8,17 +8,17 @@ class Lang(StrEnum):
     pt = 'pt-BR'
 
 
-__simulacras_new_names: list[str] = [file.removesuffix('.json') for file in listdir('src/data/en-US/simulacra_v2')]
+__simulacras_new_names: list[str] = [file.removesuffix('.json') for file in sorted(listdir('src/data/en-US/simulacra_v2'))]
 SIMULACRAS_V2 = StrEnum('SIMULACRAS_NEW', __simulacras_new_names)
 
-__simulacras_names: list[str] = [file.removesuffix('.json') for file in listdir('src/data/en-US/simulacra')]
+__simulacras_names: list[str] = [file.removesuffix('.json') for file in sorted(listdir('src/data/en-US/simulacra'))]
 SIMULACRAS = StrEnum('SIMULACRAS', __simulacras_names)
 
-__matrices_names: list[str] = [file.removesuffix('.json') for file in listdir('src/data/en-US/matrices')]
+__matrices_names: list[str] = [file.removesuffix('.json') for file in sorted(listdir('src/data/en-US/matrices'))]
 MATRICES = StrEnum('MATRICES', __matrices_names)
 
-__weapons_names: list[str] = [file.removesuffix('.json') for file in listdir('src/data/en-US/weapons')]
+__weapons_names: list[str] = [file.removesuffix('.json') for file in sorted(listdir('src/data/en-US/weapons'))]
 WEAPONS = StrEnum('WEAPONS', __weapons_names)
 
-__relics_names: list[str] = [file.removesuffix('.json') for file in listdir('src/data/en-US/relics')]
+__relics_names: list[str] = [file.removesuffix('.json') for file in sorted(listdir('src/data/en-US/relics'))]
 RELICS = StrEnum('RELICS', __relics_names)
