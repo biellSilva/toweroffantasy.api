d94da0ce988c3612c5e6e0c88b0afe3297c909d5
Gabriel Silva Nobrega <gabrieltkdnobrega63@gmail.com>
2023-11-08 14:47:48 -0300

    enabling acess log

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ DEBUG = False
 
 if __name__ == '__main__':
     if DEBUG:
-        run(app='main:app', access_log=True, log_level='debug', 
-            reload=True, reload_delay=15, workers=1)
+        run(app='main:app', access_log=True, 
+            reload=True, workers=1)
     else:
-        run(app=app, host='0.0.0.0', port=8080, access_log=False)
+        run(app=app, host='0.0.0.0', port=8080, access_log=True)
