74150249d9d25d3f769e6d8f29a5c650ae176791
Gabriel Silva Nobrega <gabrieltkdnobrega63@gmail.com>
2023-11-08 11:59:22 -0300

    fix: changed from specific image route to dynamic

--- api/core/routes/image.py
+++ api/core/routes/image.py
@@ -5,24 +5,19 @@ from fastapi import APIRouter
 from fastapi.responses import Response
 from typing import Literal
 
-from api.enums import SIMULACRAS
-
-from api.infra.repository import *
-from api.infra.entitys import EntityBase
 from api.core.exceptions import ItemNotFound
 
 
 router = APIRouter(prefix='/image', tags=['image'])
 
-SIMU_REPO = SimulacraRepo()
 
+@router.get('')
+async def image(path: str, format: Literal['png', 'webp']='webp'):
+    async with aiohttp.ClientSession() as cs:
+        path = path if '.png' in path else f'{path}.png'
 
-@router.get('/simulacra/{id}')
-async def simulacra_image(id: SIMULACRAS, format: Literal['png', 'webp']='webp'):
-    if simulacra := await SIMU_REPO.get(EntityBase(id=id), 'en'):
-        async with aiohttp.ClientSession() as cs:
-            async with cs.get(f'https://raw.githubusercontent.com/Silyky/Icon_CN/main/UI/huanxing/lihui/{simulacra.avatarID.lower()}.png') as res:
-                if res.status == 200:
-                    return Response(await res.read(), media_type=f'image/{format.upper()}')
-                else:
-                    raise ItemNotFound(headers={'error': f'Couldn\'t find {simulacra.id} image'})
+        async with cs.get(f'https://raw.githubusercontent.com/Silyky/Icon_CN/main/{path}') as res:
+            if res.status == 200:
+                return Response(await res.read(), media_type=f'image/{format.upper()}')
+            else:
+                raise ItemNotFound(headers={'error': f'Couldn\'t find {path} image'})
