e6db400edac9f87eae1a7dbc3c38d6872bea4bc4
Gabriel Silva Nobrega <gabrieltkdnobrega63@gmail.com>
2023-11-09 19:03:57 -0300

    fix: route pattern

--- api/core/routes/weapons.py
+++ api/core/routes/weapons.py
@@ -10,7 +10,7 @@ from api.infra.entitys import Weapon, EntityBase
 from api.infra.repository import WeaponRepo
 
 
-router = APIRouter(prefix='/weapons', tags=['weapons'])
+router = APIRouter(prefix='/weapon', tags=['weapon'])
 
 WEAPON_REPO = WeaponRepo()
 
