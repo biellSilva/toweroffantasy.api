08b9f0cec42d98acf512bacbc206401d4f7e0228
Gabriel Silva Nobrega <gabrieltkdnobrega63@gmail.com>
2023-11-11 13:20:46 -0300

    fix: removing acess log from live app

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ DEBUG = False
 
 if __name__ == '__main__':
     if DEBUG:
-        run(app='main:app', access_log=True, 
-            reload=True, workers=1)
+        run(app='main:app', reload=True, workers=1)
     else:
-        run(app=app, host='0.0.0.0', port=8080, access_log=True)
+        run(app=app, host='0.0.0.0', port=8080)
