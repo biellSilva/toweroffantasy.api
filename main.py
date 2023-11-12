39b3d3b856e8c8d6031ca7e551b03ce20577042e
Gabriel Silva Nobrega <gabrieltkdnobrega63@gmail.com>
2023-11-12 19:51:06 -0300

    fix: main

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from uvicorn import run # type: ignore
 
 from api import app
 
-DEBUG = True
+DEBUG = False
 
 if __name__ == '__main__':
     if DEBUG:
         run(app='main:app', reload=True, workers=1)
     else:
-        run(app=app, host='0.0.0.0', port=8080)
+        run(app=app, host='0.0.0.0', port=8080, access_log=False)
