073e30418cbc2647c8dc835901f64f82fd22c04d
Gabriel Silva Nobrega <gabrieltkdnobrega63@gmail.com>
2023-11-13 20:47:06 -0300

    fix: webp format through pillow

--- api/core/routes/image.py
+++ api/core/routes/image.py
@@ -2,8 +2,10 @@
 import aiohttp
 
 from fastapi import APIRouter
-from fastapi.responses import Response
+from fastapi.responses import Response, StreamingResponse
 from typing import Literal
+from PIL import Image
+from io import BytesIO
 
 from api.core.exceptions import ItemNotFound
 
@@ -19,6 +21,16 @@ async def get_asset(path: str, format: Literal['png', 'webp']='webp'):
 
         async with cs.get(f'https://raw.githubusercontent.com/FortOfFans/ToF.github.io/main/{path}') as res:
             if res.status == 200:
-                return Response(await res.read(), media_type=f'image/{format.upper()}')
+                if format == 'webp':
+                    image = Image.open(BytesIO(await res.read()))
+                    buffer = BytesIO()
+
+                    image.save(buffer, format='webp')
+                    buffer.seek(0)
+
+                    return StreamingResponse(buffer, media_type=f'image/{format.upper()}')
+                
+                else:
+                    return Response(await res.read(), media_type=f'image/{format.upper()}')
             else:
                 raise ItemNotFound(headers={'error': f'Couldn\'t find {path} asset'})
