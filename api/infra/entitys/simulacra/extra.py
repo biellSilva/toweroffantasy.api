811f9874bc0f2cae7d11cfb4fcac474f512e25c9
Gabriel Silva Nobrega <gabrieltkdnobrega63@gmail.com>
2023-11-16 12:18:59 -0300

    feat: added new keys to simulacra assets

--- api/infra/entitys/simulacra/extra.py
+++ api/infra/entitys/simulacra/extra.py
@@ -23,6 +23,13 @@ class Assets(BaseModel):
     artwork: Annotated[str, BeforeValidator(put_imitation_icon)] | None = Field(default=None, alias='icon', serialization_alias='artwork')
     lotteryCard: Annotated[str, BeforeValidator(replace_icon)] | None = Field(default=None, alias='LotteryCardImage', serialization_alias='lotteryCard')
     lotteryDrawing: Annotated[str, BeforeValidator(replace_icon)] | None = Field(default=None, alias='LotteryDrawing', serialization_alias='lotteryDrawing')
+    painting: Annotated[str, BeforeValidator(replace_icon)] | None = Field(default=None, alias='Painting', serialization_alias='painting')
+    grayPainting: Annotated[str, BeforeValidator(replace_icon)] | None = Field(default=None, alias='GrayPainting', serialization_alias='grayPainting')
+    namePicture: Annotated[str, BeforeValidator(replace_icon)] | None = Field(default=None, alias='NamePicture', serialization_alias='namePicture')
+    thumbPainting: Annotated[str, BeforeValidator(replace_icon)] | None = Field(default=None, alias='ThumbPainting', serialization_alias='thumbPainting')
+    weaponPicture: Annotated[str, BeforeValidator(replace_icon)] | None = Field(default=None, alias='WeaponShowPicture', serialization_alias='weaponPicture')
+    awakenEntrance: Annotated[str, BeforeValidator(replace_icon)] | None = Field(default=None, alias='HasGotAwakenEntrance', serialization_alias='awakenEntrance')
+    grayAwakenEntrance: Annotated[str, BeforeValidator(replace_icon)] | None = Field(default=None, alias='NotGotAwakenEntrance', serialization_alias='grayAwakenEntrance')
 
 
 class Banner(BaseModel):
--- api/infra/entitys/simulacra_v2/extra.py
+++ api/infra/entitys/simulacra_v2/extra.py
@@ -10,3 +10,10 @@ class Assets(BaseModel):
     artwork: Annotated[str, BeforeValidator(put_imitation_icon)] | None = None
     lotteryCard: Annotated[str, BeforeValidator(replace_icon)] | None = Field(default=None)
     lotteryDrawing: Annotated[str, BeforeValidator(replace_icon)] | None = Field(default=None)
+    painting: Annotated[str, BeforeValidator(replace_icon)] | None = Field(default=None)
+    grayPainting: Annotated[str, BeforeValidator(replace_icon)] | None = Field(default=None)
+    namePicture: Annotated[str, BeforeValidator(replace_icon)] | None = Field(default=None)
+    thumbPainting: Annotated[str, BeforeValidator(replace_icon)] | None = Field(default=None)
+    weaponPicture: Annotated[str, BeforeValidator(replace_icon)] | None = Field(default=None)
+    awakenEntrance: Annotated[str, BeforeValidator(replace_icon)] | None = Field(default=None)
+    grayAwakenEntrance: Annotated[str, BeforeValidator(replace_icon)] | None = Field(default=None)
