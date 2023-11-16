87795b0beb5407881b704d7c0ee83c771ffeb649
Gabriel Silva Nobrega <gabrieltkdnobrega63@gmail.com>
2023-11-15 21:31:50 -0300

    fix: get matrice by name if equals imitation name

--- api/infra/repository/matrice.py
+++ api/infra/repository/matrice.py
@@ -44,5 +44,5 @@ class MatriceRepo(ModelRepository[EntityBase, Matrice]):
     
     async def get_by_name(self, name: str, lang: str):
         for i in await self.get_all(lang):
-            if i.name in name:
+            if i.name == name:
                 return i
