ff49d12e8b963f2c0711cba82d39b4186b037459
Gabriel Silva Nobrega <gabrieltkdnobrega63@gmail.com>
2023-11-16 21:42:54 -0300

    fix: workers

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ from uvicorn import run # type: ignore
 
 from api import app
 
+__import__ = [
+    app
+]
+
 DEBUG = False
 
 if __name__ == '__main__':
     if DEBUG:
         run(app='main:app', reload=True, workers=1)
     else:
-        run(app=app, host='0.0.0.0', port=8080, workers=10)
+        run(app='main:app', host='0.0.0.0', port=8080, workers=10)
