b27b3210f2d439c9bb99b23d65559f36d5691e3e
Gabriel Silva Nobrega <gabrieltkdnobrega63@gmail.com>
2023-11-19 17:53:23 -0300

    fix: banner ids

--- api/infra/entitys/banners.py
+++ api/infra/entitys/banners.py
@@ -4,9 +4,9 @@ from typing import Annotated
 
 
 class Banner(BaseModel):
-    imitation_id: Annotated[str | None, BeforeValidator(lambda x: x.lower())] = Field(exclude=True, default=None)
-    weapon_id: Annotated[str | None, BeforeValidator(lambda x: x.lower())] = Field(exclude=True, default=None)
-    matrix_id: Annotated[str | None, BeforeValidator(lambda x: x.lower())] = Field(exclude=True, default=None)
+    imitation_id: Annotated[str, BeforeValidator(lambda x: x.lower())] | None = Field(exclude=True, default=None)
+    weapon_id: Annotated[str, BeforeValidator(lambda x: x.lower())] | None = Field(exclude=True, default=None)
+    matrix_id: Annotated[str, BeforeValidator(lambda x: x.lower())] | None = Field(exclude=True, default=None)
     simulacrum: str = Field(exclude=True)
     bannerNo: int
     start: str
