7dfcb18c133120b0a5a16f5428e2c8ec1b134e3f
Zakum <mirveo@gmail.com>
2023-11-19 08:05:04 -0800

    Added sort order for bannerless items (simulacra, weapons, matrices)

---
+++ api/infra/database/sort_order.js
@@ -0,0 +1,82 @@
+// Sort order for items that do not have a banner. Useful to keep a consistent order of data across languages, because sorting by name may result in a different order of data based on the language.
+
+// Cocoritter
+// Crow
+// Huma
+// KING
+// Meryl
+// Samir
+// Shiro
+// Tsubasa
+// Zero
+// Bai Ling
+// Echo
+// Ene
+// Hilda
+// Pepper
+
+export const SIMULACRA_SORT_ORDER = [
+    "imitation_11",
+    "imitation_13",
+    "imitation_6",
+    "imitation_9",
+    "imitation_3",
+    "imitation_33",
+    "imitation_5",
+    "imitation_8",
+    "imitation_7",
+    "imitation_1",
+    "imitation_16",
+    "imitation_4",
+    "imitation_2",
+    "imitation_15",
+    "imitation_14",
+]
+
+export const WEAPON_SORT_ORDER = [
+    "stave_ice",
+    "digger_thunder",
+    "shieldaxe_fire",
+    "sickle_fire",
+    "bigsword_ice",
+    // null, // Molinia
+    "dgun_thunder",
+    "darts_physic",
+    "bow_ice",
+    "cube_fire",
+    "bow_physic",
+    "spear_thunder",
+    "hammer_ice",
+    "cannon_ice",
+    "stave_thunder",
+    "digger_physic", // Combat Blade
+    "bow_fire", // Composite Bow
+    "sword_thunder", // EM Blade
+    "spear_ice" // Frosted Spear
+]
+
+export const MATRIX_SORT_ORDER = [
+    "matrix_ssr3",
+    "matrix_ssr11",
+    "matrix_ssr10",
+    "matrix_ssr4",
+    "matrix_ssr1",
+    // null, // Molinia
+    "matrix_ssr9",
+    "matrix_ssr7",
+    "matrix_ssr2",
+    "matrix_ssr8",
+    "matrix_sr9",
+    "matrix_sr12",
+    "matrix_sr8",
+    "matrix_sr7",
+    "matrix_sr6",
+    "matrix_r6", // Attack Analyzer
+    "matrix_r1", // Explosive Core
+    "matrix_r2", // Hauler
+    "matrix_r4", // Provocateurs
+    "matrix_r7", // Purge Pact
+    "matrix_r3", // Standard Guard
+    "matrix_n2", // Overheat
+    "matrix_n1", // Wandering Aberrant
+]
