5fc32f7facb301cb04f86b885d564368398bc040
Gabriel Silva Nobrega <gabrieltkdnobrega63@gmail.com>
2023-11-19 18:18:16 -0300

    sorting matrix by rarity - name - order

--- api/utils.py
+++ api/utils.py
@@ -142,8 +142,49 @@ def sort_weapons(weapon: 'Weapon') -> tuple[int, int]:
                 return 2, 0
             
     return 3, 0
+
+
+def sort_matrices(matrice: 'Matrix') -> tuple[int, str, int]:
+    if matrice.rarity == 'SSR':
+        if matrice.banners:
+            return -1, matrice.name, -matrice.banners[-1].bannerNo
+        else:
+            if matrice.id in MATRIX_SORT_ORDER:
+                return -1, matrice.name, MATRIX_SORT_ORDER.index(matrice.id)
+            else:
+                return -1,  matrice.name, 0
     
-def sort_matrices(matrice: 'Matrix') -> tuple[int, int, str]:
+    elif matrice.rarity == 'SR':
+        if matrice.banners:
+            return 1, matrice.name, -matrice.banners[-1].bannerNo
+        else:
+            if matrice.id in MATRIX_SORT_ORDER:
+                return 1, matrice.name, MATRIX_SORT_ORDER.index(matrice.id)
+            else:
+                return 1,  matrice.name, 0
+    
+    elif matrice.rarity == 'R':
+        if matrice.banners:
+            return 2, matrice.name, -matrice.banners[-1].bannerNo
+        else:
+            if matrice.id in MATRIX_SORT_ORDER:
+                return 2, matrice.name, MATRIX_SORT_ORDER.index(matrice.id)
+            else:
+                return 2,  matrice.name, 0
+
+    elif matrice.rarity == 'N':
+        if matrice.banners:
+            return 3, matrice.name, -matrice.banners[-1].bannerNo
+        else:
+            if matrice.id in MATRIX_SORT_ORDER:
+                return 3, matrice.name, MATRIX_SORT_ORDER.index(matrice.id)
+            else:
+                return 3,  matrice.name, 0
+    
+    return 4,  matrice.name, 0
+
+
+def sort_matrices_(matrice: 'Matrix') -> tuple[int, int, str]:
     if matrice.rarity == 'SSR':
         if matrice.banners:
             return -1, -matrice.banners[-1].bannerNo, matrice.name
