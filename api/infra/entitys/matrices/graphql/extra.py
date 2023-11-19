278cac9d717bba58fd2aff2e871e0ebfbb2f2f84
Gabriel Silva Nobrega <gabrieltkdnobrega63@gmail.com>
2023-11-19 17:59:11 -0300

    fix: adding weapon and matrix banners to graphql

--- api/infra/entitys/matrices/graphql/extra.py
+++ api/infra/entitys/matrices/graphql/extra.py
@@ -7,7 +7,20 @@ class MatriceSet:
     need: int | None
     description:str | None
 
+
 @strawberry.type
 class MatrixAssets:
     icon: str
     iconLarge: str
+
+
+@strawberry.type
+class Banner:
+    bannerNo: int
+    start: str
+    end: str
+    details_link: str
+    limited_banner_only: bool
+    is_rerun: bool
+    final_rerun: bool
+    is_collab: bool
