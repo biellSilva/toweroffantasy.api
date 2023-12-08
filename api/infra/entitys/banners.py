a47eb6feae32318f13a4c3284e6cc50986480e88
Gabriel Silva Nobrega <gabrieltkdnobrega63@gmail.com>
2023-12-08 08:45:47 -0300

    remove: unused object

--- api/infra/entitys/banners.py
+++ api/infra/entitys/banners.py
@@ -4,21 +4,6 @@ from typing import Annotated
 
 
 class Banner(BaseModel):
-    imitation_id: Annotated[str, BeforeValidator(lambda x: str(x).lower())] | None = Field(default=None, serialization_alias='simulacrumId')
-    weapon_id: Annotated[str, BeforeValidator(lambda x: str(x).lower())] | None = Field(default=None, serialization_alias='weaponId')
-    matrix_id: Annotated[str, BeforeValidator(lambda x: str(x).lower())] | None = Field(default=None, serialization_alias='matrixId')
-    simulacrum: str | None = Field(default=None, serialization_alias='simulacrumName')
-    bannerNo: int = Field(serialization_alias='BannerNumber')
-    start: str = Field(serialization_alias='startDate')
-    end: str = Field(serialization_alias='endDate')
-    details_link: str = Field(serialization_alias='detailsLink')
-    limited_banner_only: bool = Field(serialization_alias='isLimitedBannerOnly')
-    is_rerun: bool = Field(serialization_alias='isRerun')
-    final_rerun: bool = Field(serialization_alias='isFinalBanner')
-    is_collab: bool = Field(serialization_alias='isCollab')
-
-
-class Banner(BaseModel):
     simulacrumId: Annotated[str, BeforeValidator(lambda x: str(x).lower())] | None = Field(default=None, alias='imitation_id')
     weaponId: Annotated[str, BeforeValidator(lambda x: str(x).lower())] | None = Field(default=None, alias='weapon_id')
     matrixId: Annotated[str, BeforeValidator(lambda x: str(x).lower())] | None = Field(default=None, alias='matrix)_id')
