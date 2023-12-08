6117c587f8b8182addb2894fcbb33d012b3cf4e2
Gabriel Silva Nobrega <gabrieltkdnobrega63@gmail.com>
2023-12-08 06:33:37 -0300

    rename: renaming keys

--- api/infra/entitys/banners.py
+++ api/infra/entitys/banners.py
@@ -1,19 +1,19 @@
 
-from pydantic import BaseModel, BeforeValidator
+from pydantic import BaseModel, BeforeValidator, Field
 from typing import Annotated
 
 
 class Banner(BaseModel):
-    imitation_id: Annotated[str, BeforeValidator(lambda x: x.lower())] | None = None
-    weapon_id: Annotated[str, BeforeValidator(lambda x: x.lower())] | None = None
-    matrix_id: Annotated[str, BeforeValidator(lambda x: x.lower())] | None = None
-    simulacrum: str | None = None
-    bannerNo: int
-    start: str
-    end: str
-    details_link: str
-    limited_banner_only: bool
-    is_rerun: bool
-    final_rerun: bool
-    is_collab: bool
+    imitation_id: Annotated[str, BeforeValidator(lambda x: str(x).lower())] | None = Field(default=None, serialization_alias='simulacrumId')
+    weapon_id: Annotated[str, BeforeValidator(lambda x: str(x).lower())] | None = Field(default=None, serialization_alias='weaponId')
+    matrix_id: Annotated[str, BeforeValidator(lambda x: str(x).lower())] | None = Field(default=None, serialization_alias='matrixId')
+    simulacrum: str | None = Field(default=None, serialization_alias='simulacrumName')
+    bannerNo: int = Field(serialization_alias='BannerNumber')
+    start: str = Field(serialization_alias='startDate')
+    end: str = Field(serialization_alias='endDate')
+    details_link: str = Field(serialization_alias='detailsLink')
+    limited_banner_only: bool = Field(serialization_alias='isLimitedBannerOnly')
+    is_rerun: bool = Field(serialization_alias='isRerun')
+    final_rerun: bool = Field(serialization_alias='isFinalBanner')
+    is_collab: bool = Field(serialization_alias='isCollab')
 
--- api/infra/entitys/base.py
+++ api/infra/entitys/base.py
@@ -1,7 +1,35 @@
 
-from pydantic import BaseModel, BeforeValidator
-from typing import Annotated
+from pydantic import BaseModel, BeforeValidator, root_validator
+from typing import Annotated, Any
 
 
 class EntityBase(BaseModel):
-    id: Annotated[str, BeforeValidator(lambda x: x.lower())]
+    id: Annotated[str, BeforeValidator(lambda x: str(x).lower())]
+
+
+    @root_validator(pre=True)
+    def __lowercase_property_keys__(cls, values: Any) -> Any:
+        def __lower__(value: Any) -> Any:
+            if isinstance(value, dict):
+                return {k.lower(): __lower__(v) for k, v in value.items()} # type: ignore
+            return value
+
+        return __lower__(values)
+
+
+    def custom_model_dump(self, include: set[str] | None = None):
+        return self.__pascal_to_camel_dict(self.model_dump(include=include, by_alias=True))
+
+
+    def __pascal_to_camel_dict(self, d: dict[Any, Any]) -> dict[str, Any]:
+        def pascal_to_camel(string: str) -> str:
+            return string[0].lower() + string[1:]
+
+        def process_value(v: Any):
+            if isinstance(v, dict):
+                return self.__pascal_to_camel_dict(v) # type: ignore
+            
+            else:
+                return v
+
+        return {pascal_to_camel(k): process_value(v) for k, v in d.items()}
