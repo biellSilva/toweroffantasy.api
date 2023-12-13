e09628af52db1664a32ed235a89ee0084e3abfd6
Gabriel Silva Nobrega <gabrieltkdnobrega63@gmail.com>
2023-12-13 19:23:08 -0300

    fix: serialization alias

--- api/infra/entitys/simulacra/simulacra.py
+++ api/infra/entitys/simulacra/simulacra.py
@@ -19,16 +19,16 @@ class Simulacra(EntityBase):
     weaponId: Annotated[str | None, BeforeValidator(lambda x: None if not x or (isinstance(x, str) and (x.lower() == 'none' or x.lower() == 'null')) else x)] = None 
     matrixId: Annotated[str | None, BeforeValidator(lambda x: None if not x or (isinstance(x, str) and (x.lower() == 'none' or x.lower() == 'null')) else x)] = None
 
-    likedGiftTypes: list[str] = Field(alias='like')
-    # dislikedGiftTypes: list[str] = Field(alias='dislike')
+    likedGiftTypes: list[str] = Field(validation_alias=AliasChoices('like', 'likedGiftTypes'))
+    # dislikedGiftTypes: list[str] = Field(validation_alias='dislike')
     gender: str | None = None
-    birthday: str | None = Field(default=None, alias='age')
+    birthday: str | None = Field(default=None, validation_alias=AliasChoices('age', 'birthday'))
     # Age: str | None = None
     # Title: str | None = None
     # Job: str | None = None
     height: str | None = None
-    affiliation: str | None = Field(default=None, alias='state')
-    homeTown: str | None = Field(default=None, alias='city')
+    affiliation: str | None = Field(default=None, validation_alias=AliasChoices('state', 'affiliation'))
+    homeTown: str | None = Field(default=None, validation_alias=AliasChoices('city', 'homeTown'))
     assetsA0: Assets
     assetsA3: Assets | None = None
     voicing: VC = Field(validation_alias=AliasChoices('va', 'voicing'))
