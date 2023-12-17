703c7efd9fc2a3df6434ee23c8ba10e2e194c8ed
Gabriel Silva Nobrega <gabrieltkdnobrega63@gmail.com>
2023-12-17 17:42:33 -0300

    fix: mounts and servants model

--- api/infra/entitys/servants/servant.py
+++ api/infra/entitys/servants/servant.py
@@ -11,9 +11,9 @@ from api.utils import pet_material_rework
 class SmartServant(EntityBase):
     name: str
     description: str
-    rarity: str = Field(alias='PetRarity', serialization_alias='rarity')
-    element: str = Field(alias='PetElementType', serialization_alias='element')
-    type: str = Field(alias='PetPropertyType', serialization_alias='type')
+    rarity: str = Field(validation_alias='petRarity')
+    element: str = Field(validation_alias='petElementType')
+    type: str = Field(validation_alias='petPropertyType')
     assets: ServantAsset
     skills: list[ServantSkill]
-    upgradeItems: Annotated[list[ServantUpgrade], BeforeValidator(pet_material_rework)] = Field(alias='PetUpgradeItemMap', serialization_alias='upgradeItems')
+    upgradeItems: Annotated[list[ServantUpgrade], BeforeValidator(pet_material_rework)] = Field(validation_alias='petUpgradeItemMap')
