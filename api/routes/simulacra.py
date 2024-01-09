8193412fd134e8178d415875cd447308ca7f2e20
Gabriel <gabrieltkdnobrega63@gmail.com>
2024-01-09 18:55:55 -0300

    style: descriptions

--- api/routes/simulacra.py
+++ api/routes/simulacra.py
@@ -1,5 +1,5 @@
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Path, Query
 from fastapi.responses import ORJSONResponse
 
 from api.enums import SIMULACRA, SIMULACRA_CN, LANGS, LANGS_CN, VERSIONS
@@ -14,49 +14,22 @@ SIMU_REPO = SimulacraRepo()
 router = APIRouter(prefix='/simulacra', tags=['Simulacra'])
 METADATA = {
     'name': 'Simulacra',
-    'description': 'Simulacra are the player\'s representation of the characters found in Tower of Fantasy \n\n **CONTAINS CN DATA**',
+    'description': ('Simulacra are the player\'s representation of the characters found in Tower of Fantasy \t\n'
+                    'CONTAINS CN DATA'),
     }
 
 INCLUDE = {'id', 'name', 'assetsA0', 'weaponId', 'matrixId', 'rarity'}
 
 
 @router.get('/{id}', name='Get simulacrum', response_model=Simulacra)
-async def get_simulacrum(id: SIMULACRA | SIMULACRA_CN, 
-                        #  version: VERSIONS = VERSIONS('global'),
-                         lang: LANGS | LANGS_CN = LANGS('en'), 
-                         include: bool = True):
+async def get_simulacrum(id: SIMULACRA = Path(description='Imitation/Simulacrum ID'), 
+                        #  version: VERSIONS = Query(VERSIONS('global'), description='Game Version'),
+                         lang: LANGS = Query(LANGS('en'), description='Language code'), 
+                         include: bool = Query(True, description='Include all data keys')
+):
     '''
-    **Path Param** \n
-        id: 
-            type: string
-            required: True
-            desc: Imitation/Simulacrum ID
-            schema: SIMULACRA | SIMULACRA_CN
-
-
-    **Query Params** \n
-        version (DISABLED):
-            type: string
-            default: global
-            desc: Game version
-            schema: VERSIONS
-
-        lang:
-            type: string
-            default: en
-            desc: Possible languages
-            schema: LANGS | LANGS_CN
-
-        include:
-            type: bool
-            default: True
-            desc: Include all data keys
-        
-        
-            
     **Return** \n
         Simulacra
-
     '''
     
     simulacra = await SIMU_REPO.get(EntityBase(id=id), lang, version=VERSIONS('global'))
@@ -69,37 +42,13 @@ async def get_simulacrum(id: SIMULACRA | SIMULACRA_CN,
 
 
 @router.get('', name='All simulacra', response_model=list[Simulacra])
-async def get_all_simulacra(version: VERSIONS = VERSIONS('global'), 
-                            lang: LANGS | LANGS_CN = LANGS('en'), 
-                            include: bool = False,
-                            includeUnreleased: bool = False):
+async def get_all_simulacra(version: VERSIONS = Query(VERSIONS('global'), description='Game Version'), 
+                            lang: LANGS = Query(LANGS('en'), description='Language code'), 
+                            include: bool = Query(False, description='Include all data keys'), 
+                            includeUnreleased: bool = Query(False, description='Include unreleased data')):
     '''
-    **Query Params** \n
-        version:
-            type: str
-            default: global
-            desc: Game version
-            schema: VERSIONS
-        
-        lang:
-            type: string
-            default: en
-            desc: Possible languages
-            schema: LANGS | LANGS_CN
-    
-        include:
-            type: bool
-            default: False
-            desc: Include all data keys
-
-        includeUnreleased:
-            type: bool
-            default: False
-            desc: Only released data
-            
     **Return** \n
         List[Simulacra]
-
     '''
     
     simulacra = await SIMU_REPO.get_all(lang=lang, version=version)
