2c269d6f6d04f9587b7b671574a0a9d65af9e9e5
Gabriel Nobrega <gabrieltkdnobrega63@gmail.com>
2024-02-22 16:40:17 -0300

    feat(simulacra): add guidebook info

--- api/infra/entitys/simulacra/simulacra.py
+++ api/infra/entitys/simulacra/simulacra.py
@@ -1,35 +1,71 @@
-
-from pydantic import Field, BeforeValidator, AliasChoices
 from typing import Annotated
 
-from api.infra.entitys.base import EntityBase
+from pydantic import AliasChoices, BeforeValidator, Field
 
-from .extra import VoiceActors as VC, Awakening, Assets, Fashion
+from api.infra.entitys.base import EntityBase
+from api.infra.entitys.guidebooks.extra import GuideBookItem
+
 from ..banners import Banner
+from .extra import Assets, Awakening, Fashion
+from .extra import VoiceActors as VC
 
 
 class Simulacra(EntityBase):
-    name: str = 'unknown'
+    name: str = "unknown"
     rarity: int
-    version: str = 'unknown'
+    version: str = "unknown"
     isReleased: bool = True
-    avatarId: str 
+    avatarId: str
     advanceId: str | None = None
     unlockInfo: str
-    
-    weaponId: Annotated[str | None, BeforeValidator(lambda x: None if not x or (isinstance(x, str) and (x.lower() == 'none' or x.lower() == 'null')) else x.lower())] = None 
-    matrixId: Annotated[str | None, BeforeValidator(lambda x: None if not x or (isinstance(x, str) and (x.lower() == 'none' or x.lower() == 'null')) else x.lower())] = None
 
-    likedGiftTypes: list[str] = Field(default=[], validation_alias=AliasChoices('like', 'likedGiftTypes'))
-    dislikedGiftTypes: list[str] = Field(default=[], validation_alias=AliasChoices('dislike', 'dislikedGiftTypes'))
+    weaponId: Annotated[
+        str | None,
+        BeforeValidator(
+            lambda x: (
+                None
+                if not x
+                or (isinstance(x, str) and (x.lower() == "none" or x.lower() == "null"))
+                else x.lower()
+            )
+        ),
+    ] = None
+    matrixId: Annotated[
+        str | None,
+        BeforeValidator(
+            lambda x: (
+                None
+                if not x
+                or (isinstance(x, str) and (x.lower() == "none" or x.lower() == "null"))
+                else x.lower()
+            )
+        ),
+    ] = None
+
+    likedGiftTypes: list[str] = Field(
+        default=[], validation_alias=AliasChoices("like", "likedGiftTypes")
+    )
+    dislikedGiftTypes: list[str] = Field(
+        default=[], validation_alias=AliasChoices("dislike", "dislikedGiftTypes")
+    )
     gender: str | None = None
-    birthday: str | None = Field(default=None, validation_alias=AliasChoices('age', 'birthday'))
+    birthday: str | None = Field(
+        default=None, validation_alias=AliasChoices("age", "birthday")
+    )
     height: str | None = None
-    affiliation: str | None = Field(default=None, validation_alias=AliasChoices('state', 'affiliation'))
-    homeTown: str | None = Field(default=None, validation_alias=AliasChoices('city', 'homeTown'))
+    affiliation: str | None = Field(
+        default=None, validation_alias=AliasChoices("state", "affiliation")
+    )
+    homeTown: str | None = Field(
+        default=None, validation_alias=AliasChoices("city", "homeTown")
+    )
     assetsA0: Assets
     assetsA3: Assets | None = None
-    voicing: VC = Field(validation_alias=AliasChoices('va', 'voicing'))
+    voicing: VC = Field(validation_alias=AliasChoices("va", "voicing"))
     awakening: list[Awakening]
     banners: list[Banner] = []
     fashion: list[Fashion] = []
+    guidebook: Annotated[
+        list[GuideBookItem],
+        Field(default=[], validation_alias=AliasChoices("nitai", "guidebook")),
+    ]
