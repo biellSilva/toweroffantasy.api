68deed28540297bdd95d1930af24448883e13829
Gabriel Nobrega <gabrieltkdnobrega63@gmail.com>
2024-02-22 18:14:44 -0300

    fix(banners): fix brevey element

--- api/infra/entitys/banners.py
+++ api/infra/entitys/banners.py
@@ -1,4 +1,3 @@
-
 from pydantic import BaseModel, BeforeValidator, Field, model_validator, AliasChoices
 from typing import Annotated, Any
 from pathlib import Path
@@ -6,40 +5,61 @@ from json import loads
 from datetime import datetime
 
 
-weapons_data: dict[str, Any] = loads(Path('api/infra/database/global/en/weapons.json').read_bytes())
+weapons_data: dict[str, Any] = loads(
+    Path("api/infra/database/global/en/weapons.json").read_bytes()
+)
 
 
 class Banner(BaseModel):
-    simulacrumId: Annotated[str, BeforeValidator(lambda x: str(x).lower())] | None = Field(default=None, validation_alias=AliasChoices('imitation_id', 'simulacrumId'))
-    weaponId: Annotated[str, BeforeValidator(lambda x: str(x).lower())] | None = Field(default=None, validation_alias=AliasChoices('weapon_id', 'weaponId'))
-    matrixId: Annotated[str, BeforeValidator(lambda x: str(x).lower())] | None = Field(default=None, validation_alias=AliasChoices('matrix_id', 'matrixId'))
-    simulacrumName: str | None = Field(default=None, validation_alias=AliasChoices('simulacrum', 'simulacrumName'))
-    bannerNumber: int = Field(validation_alias=AliasChoices('bannerNo', 'bannerNumber'))
+    simulacrumId: Annotated[str, BeforeValidator(lambda x: str(x).lower())] | None = (
+        Field(
+            default=None, validation_alias=AliasChoices("imitation_id", "simulacrumId")
+        )
+    )
+    weaponId: Annotated[str, BeforeValidator(lambda x: str(x).lower())] | None = Field(
+        default=None, validation_alias=AliasChoices("weapon_id", "weaponId")
+    )
+    matrixId: Annotated[str, BeforeValidator(lambda x: str(x).lower())] | None = Field(
+        default=None, validation_alias=AliasChoices("matrix_id", "matrixId")
+    )
+    simulacrumName: str | None = Field(
+        default=None, validation_alias=AliasChoices("simulacrum", "simulacrumName")
+    )
+    bannerNumber: int = Field(validation_alias=AliasChoices("bannerNo", "bannerNumber"))
     element: str | None = None
     category: str | None = None
-    startDate: str = Field(validation_alias=AliasChoices('start', 'startDate'))
-    endDate: str = Field(validation_alias=AliasChoices('end', 'endDate'))
-    detailsLink: str = Field(validation_alias=AliasChoices('details_link', 'detailsLink'))
-    isLimitedBannerOnly: bool = Field(validation_alias=AliasChoices('limited_banner_only', 'isLimitedBannerOnly'))
-    isRerun: bool = Field(validation_alias=AliasChoices('is_rerun', 'isRerun'))
-    isFinalBanner: bool = Field(validation_alias=AliasChoices('final_rerun', 'isFinalBanner'))
-    isCollab: bool = Field(validation_alias=AliasChoices('is_collab', 'isCollab'))
-    noWeapon: bool = Field(validation_alias=AliasChoices('no_weapon', 'noWeapon'))
+    startDate: str = Field(validation_alias=AliasChoices("start", "startDate"))
+    endDate: str = Field(validation_alias=AliasChoices("end", "endDate"))
+    detailsLink: str = Field(
+        validation_alias=AliasChoices("details_link", "detailsLink")
+    )
+    isLimitedBannerOnly: bool = Field(
+        validation_alias=AliasChoices("limited_banner_only", "isLimitedBannerOnly")
+    )
+    isRerun: bool = Field(validation_alias=AliasChoices("is_rerun", "isRerun"))
+    isFinalBanner: bool = Field(
+        validation_alias=AliasChoices("final_rerun", "isFinalBanner")
+    )
+    isCollab: bool = Field(validation_alias=AliasChoices("is_collab", "isCollab"))
+    noWeapon: bool = Field(validation_alias=AliasChoices("no_weapon", "noWeapon"))
     isReleased: bool = True
 
-    @model_validator(mode='before')
+    @model_validator(mode="before")
     def add_element_and_category(cls, value: dict[str, Any]):
-        if weapon_data := weapons_data.get(value.get('weapon_id', ''), None):
-            value['element'] = weapon_data['element']
-            value['category'] = weapon_data['wc']
+        if weapon_data := weapons_data.get(value.get("weapon_id", ""), None):
+            value["element"] = weapon_data["element"]
+            value["category"] = weapon_data["wc"]
+
+            if value["weapon_id"] == "blevi_thunder":
+                value["element"] = "IceThunder"
 
         try:
-            if datetime.strptime(value['start'], '%Y/%m/%d %H:%M') > datetime.now():
-                value['isReleased'] = False
+            if datetime.strptime(value["start"], "%Y/%m/%d %H:%M") > datetime.now():
+                value["isReleased"] = False
         except:
             try:
-                if datetime.strptime(value['start'], '%Y/%m/%d') > datetime.now():
-                    value['isReleased'] = False
+                if datetime.strptime(value["start"], "%Y/%m/%d") > datetime.now():
+                    value["isReleased"] = False
             except:
                 pass
 
