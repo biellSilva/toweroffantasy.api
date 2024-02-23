9016e013fa64d75f47ab8851d5f85076a9888810
Gabriel Nobrega <gabrieltkdnobrega63@gmail.com>
2024-02-23 00:35:19 -0300

    feat(domain-usecase): add usecase interface

---
+++ src/domain/usecases/base/__init__.py
@@ -0,0 +1,13 @@
+from abc import ABC, abstractmethod
+from typing import Generic, TypeVar
+
+from pydantic import BaseModel
+
+BMI = TypeVar("BMI", bound=BaseModel)
+BMO = TypeVar("BMO", bound=BaseModel)
+
+
+class IUsecase(ABC, Generic[BMI, BMO]):
+
+    @abstractmethod
+    async def execute(self, params: BMI) -> BMO: ...
