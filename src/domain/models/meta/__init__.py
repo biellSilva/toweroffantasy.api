e1abec380a2e26a5c74ab24b1032b2df1714e40d
Gabriel Nobrega <gabrieltkdnobrega63@gmail.com>
2024-02-23 02:56:32 -0300

    feat(models): add metadata model

---
+++ src/domain/models/meta/__init__.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel
+
+
+class MetaLastUpdated(BaseModel):
+    username: str = "unknown"
+    timestamp: int = 0
+
+
+class RecoMatrix(BaseModel):
+    id: str
+    pieces: int
+
+
+class MetaData(BaseModel):
+    recommendedPairings: list[str] = []
+    recommendedMatrices: list[RecoMatrix] = []
+    rating: list[float] = []
+    analyticVideoId: str | None = None
+    lastUpdated: MetaLastUpdated = MetaLastUpdated()
