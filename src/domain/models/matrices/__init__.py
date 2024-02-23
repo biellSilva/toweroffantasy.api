2cab9c6c8b8485da9722010c3b517ddccaa1c004
Gabriel Nobrega <gabrieltkdnobrega63@gmail.com>
2024-02-23 17:35:06 -0300

    feat(models): add matrix model

---
+++ src/domain/models/matrices/__init__.py
@@ -0,0 +1,19 @@
+from typing import Annotated
+from pydantic import AliasChoices, BeforeValidator, Field
+from src.domain.models.banner import Banner
+from src.domain.models.base import ModelBase
+from src.domain.models.matrices.extra import MatrixAsset, MatrixMeta, MatrixSet
+from src.utils import bold_numbers
+
+
+class Matrix(ModelBase):
+    name: str
+    description: Annotated[str, BeforeValidator(bold_numbers)]
+    assets: MatrixAsset
+    rarity: int
+    sets: list[MatrixSet] = Field(validation_alias=AliasChoices("sets", "set"))
+    version: str
+    weaponId: str | None
+    simulacrumId: str | None = None
+    banners: list[Banner] = []
+    meta: MatrixMeta
---
+++ src/domain/models/matrices/extra.py
@@ -0,0 +1,21 @@
+from typing import Annotated
+from pydantic import AliasChoices, BaseModel, BeforeValidator, Field
+
+from src.utils import bold_numbers
+
+
+class MatrixAsset(BaseModel):
+    icon: str | None = Field(validation_alias=AliasChoices("itemIcon", "icon"))
+    itemLarge: str | None = Field(
+        validation_alias=AliasChoices("itemIconLarge", "iconLarge")
+    )
+    characterArtwork: str | None = None
+
+
+class MatrixSet(BaseModel):
+    need: int
+    description: Annotated[str, BeforeValidator(bold_numbers)]
+
+
+class MatrixMeta(BaseModel):
+    recommendedWeapons: list[str] = []
