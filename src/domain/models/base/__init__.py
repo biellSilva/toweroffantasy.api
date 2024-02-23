3efd63eecbda7dc397e45bbb38037fc0f18815ad
Gabriel Nobrega <gabrieltkdnobrega63@gmail.com>
2024-02-22 23:25:10 -0300

    style(model): typing vars

--- src/domain/models/base/__init__.py
+++ src/domain/models/base/__init__.py
@@ -1,17 +1,28 @@
 from typing import Annotated, Any
+
 from pydantic import BaseModel, BeforeValidator, model_validator
+
+from src.utils import to_lowercase
 
 
 class ModelBase(BaseModel):
-    id: Annotated[str, BeforeValidator(lambda x: str(x).lower())]
+    id: Annotated[str, BeforeValidator(to_lowercase)]
 
     @model_validator(mode="before")
     def _camelcase_property_keys__(cls, values: Any) -> Any:
         def _camel__(value: Any) -> Any:
 
             if isinstance(value, dict):
-                return {str(k)[0].lower() + k[1:]: _camel__(v) for k, v in value.items() if k != ""}  # type: ignore
+                return {
+                    cls.__to_camel(key): _camel__(value_)
+                    for key, value_ in value.items()  # type: ignore
+                    if isinstance(key, str) and key != ""
+                }
 
             return value
 
         return _camel__(values)
+
+    @classmethod
+    def __to_camel(cls, text: str) -> str:
+        return text[0].lower() + text[1:]
