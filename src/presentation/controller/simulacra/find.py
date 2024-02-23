0998d806def83a85da024f1e7f463338bde3c6fc
Gabriel Nobrega <gabrieltkdnobrega63@gmail.com>
2024-02-23 01:12:05 -0300

    feat(presentation-controller): add find simulacra

--- src/presentation/controller/simulacra/find.py
+++ src/presentation/controller/simulacra/find.py
@@ -3,12 +3,21 @@ from src.domain.usecases.simulacra.find import (
     FindSimulacraResult,
     IFindSimulacraUseCase,
 )
+from src.enums import LANGS_CHINA_ENUM, LANGS_GLOBAL_ENUM, VERSIONS_ENUM
+from src.enums.simulacra import SIMULACRAS_GLOBAL_ENUM
 
 
 class FindSimulacraController:
     def __init__(self, usecase: IFindSimulacraUseCase):
         self.usecase = usecase
 
-    async def handle(self, params: FindSimulacraParams) -> FindSimulacraResult:
+    async def handle(
+        self,
+        id: SIMULACRAS_GLOBAL_ENUM,
+        version: VERSIONS_ENUM = VERSIONS_ENUM("global"),
+        lang: LANGS_GLOBAL_ENUM | LANGS_CHINA_ENUM = LANGS_GLOBAL_ENUM("en"),
+    ) -> FindSimulacraResult:
         "Find simulacra based on ID"
-        return await self.usecase.execute(params)
+        return await self.usecase.execute(
+            FindSimulacraParams(id=id, version=version, lang=lang)
+        )
