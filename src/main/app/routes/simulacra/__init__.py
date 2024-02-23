b754a3aa813c23b69f6727d8b2b831ea49afa0c6
Gabriel Nobrega <gabrieltkdnobrega63@gmail.com>
2024-02-23 01:13:41 -0300

    feat(app-route): add find simulacra route

---
+++ src/main/app/routes/simulacra/__init__.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter
+
+from src.main.factories.controller.simulacra.find import FindSimulacraControllerFactory
+
+
+router = APIRouter(prefix="/simulacra", tags=["Simulacra"])
+
+find = FindSimulacraControllerFactory.create()
+
+router.add_api_route(
+    path="/{id}", endpoint=find.handle, methods=["GET"], name="Find simulacrum"
+)
