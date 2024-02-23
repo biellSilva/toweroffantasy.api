8b2c6f4258d0bdcb4babe58757af3c14fc7e6320
Gabriel Nobrega <gabrieltkdnobrega63@gmail.com>
2024-02-23 01:50:49 -0300

    feat(domain-errors): add custom http errors

---
+++ src/domain/errors/http.py
@@ -0,0 +1,33 @@
+from typing import Any, Dict
+
+from fastapi import HTTPException, status
+
+
+class NotFoundErr(HTTPException):
+    def __init__(
+        self,
+        status_code: int = status.HTTP_404_NOT_FOUND,
+        detail: Any = "ID not found",
+        headers: Dict[str, str] | None = None,
+    ) -> None:
+        super().__init__(status_code, detail, headers)
+
+
+class DataNotFoundErr(HTTPException):
+    def __init__(
+        self,
+        status_code: int = status.HTTP_404_NOT_FOUND,
+        detail: Any = "Impossible to reach lang or version data path",
+        headers: Dict[str, str] | None = None,
+    ) -> None:
+        super().__init__(status_code, detail, headers)
+
+
+class DataIncompleteErr(HTTPException):
+    def __init__(
+        self,
+        status_code: int = status.HTTP_404_NOT_FOUND,
+        detail: Any = "While loading data files something went wrong, most likely due to a missing file",
+        headers: Dict[str, str] | None = None,
+    ) -> None:
+        super().__init__(status_code, detail, headers)
