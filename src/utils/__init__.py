5e77795ce3e8aa22f5ca41502eaf3bf63949fe26
Gabriel Nobrega <gabrieltkdnobrega63@gmail.com>
2024-02-23 01:01:12 -0300

    feat(utils): add current time function

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,6 +1,15 @@
+from datetime import datetime
+
+from pytz import timezone
+
+
 def to_lowercase(text: str) -> str:
     return text.lower().strip()
 
 
 def to_strip(text: str) -> str:
     return text.strip()
+
+
+def current_time() -> datetime:
+    return datetime.now(timezone("UTC"))
