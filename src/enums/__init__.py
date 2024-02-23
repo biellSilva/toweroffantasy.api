4f4ecc0d68d49e4c90166cf3d9870a04037b67e8
Gabriel Nobrega <gabrieltkdnobrega63@gmail.com>
2024-02-23 01:12:27 -0300

    fixenums): add enums paths

--- src/enums/__init__.py
+++ src/enums/__init__.py
@@ -1,13 +1,23 @@
 from enum import StrEnum
 from pathlib import Path
 
-__versions: list[str] = [folder.name.lower() for folder in Path("").iterdir()]
+__versions: list[str] = [
+    folder.name.lower()
+    for folder in Path("./src/infra/database").iterdir()
+    if folder.is_dir()
+]
 VERSIONS_ENUM = StrEnum("VERSIONS_ENUM", __versions)
 
 
-__langs_global: list[str] = [folder.name.lower() for folder in Path("").iterdir()]
+__langs_global: list[str] = [
+    folder.name.lower()
+    for folder in Path("./src/infra/database/global").iterdir()
+    if folder.is_dir()
+]
 LANGS_GLOBAL_ENUM = StrEnum("LANGS_GLOBAL_ENUM", __langs_global)
 
 
-__langs_china: list[str] = [folder.name.lower() for folder in Path("").iterdir()]
+__langs_china: list[str] = [
+    folder.name.lower() for folder in Path("./src/infra/database/china").iterdir()
+]
 LANGS_CHINA_ENUM = StrEnum("LANGS_CHINA_ENUM", __langs_china)
--- src/enums/simulacra.py
+++ src/enums/simulacra.py
@@ -4,7 +4,9 @@ from pathlib import Path
 
 __simulacras_global: list[str] = [
     key.lower()
-    for key in dict(json.loads(Path("").read_bytes())).keys()
+    for key in dict(
+        json.loads(Path("src/infra/database/global/en/imitations.json").read_bytes())
+    ).keys()
     if isinstance(key, str)
 ]
 SIMULACRAS_GLOBAL_ENUM = StrEnum("SIMULACRAS_GLOBAL_ENUM", __simulacras_global)
