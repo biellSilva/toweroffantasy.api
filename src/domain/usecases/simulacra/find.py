b1e29151c9227013b04d43773f9817011aa83e30
Gabriel Nobrega <gabrieltkdnobrega63@gmail.com>
2024-02-23 01:14:55 -0300

    fix(domain-usecases): find simulacra params and result

--- src/domain/usecases/simulacra/find.py
+++ src/domain/usecases/simulacra/find.py
@@ -1,23 +1,23 @@
 from abc import ABC, abstractmethod
-from fastapi import Query
+from fastapi import Path, Query
 
 from pydantic import BaseModel
 from src.domain.models.simulacra import Simulacra
 
 from src.domain.usecases.base import IUsecase
-from src.enums import LANGS_GLOBAL_ENUM, VERSIONS_ENUM
+from src.enums import LANGS_CHINA_ENUM, LANGS_GLOBAL_ENUM, VERSIONS_ENUM
 
 
 class FindSimulacraParams(BaseModel):
-    id: str
+    id: str = Path(description="Simulacrum ID")
     version: VERSIONS_ENUM = Query(VERSIONS_ENUM("global"), description="Game version")
-    lang: LANGS_GLOBAL_ENUM = Query(
+    lang: LANGS_GLOBAL_ENUM | LANGS_CHINA_ENUM = Query(
         LANGS_GLOBAL_ENUM("en"), description="Data language"
     )
 
 
-class FindSimulacraResult(BaseModel):
-    result: Simulacra
+class FindSimulacraResult(Simulacra):
+    pass
 
 
 class IFindSimulacraUseCase(IUsecase[FindSimulacraParams, FindSimulacraResult], ABC):
