7e620bed3886c522236cc4987ccb29dbac8f8f45
Gabriel Nobrega <gabrieltkdnobrega63@gmail.com>
2024-03-01 17:07:31 -0300

    fix(domain-usecases): add basemodel to generic usecase interface

--- src/domain/usecases/base/__init__.py
+++ src/domain/usecases/base/__init__.py
@@ -1,12 +1,13 @@
 from abc import ABC, abstractmethod
-from typing import Any, Generic, TypeVar
+from typing import Generic, TypeVar
 
 from pydantic import BaseModel
 
 BMI = TypeVar("BMI", bound=BaseModel)
+BMO = TypeVar("BMO", bound=BaseModel)
 
 
-class IUsecase(ABC, Generic[BMI]):
+class IUsecase(ABC, Generic[BMI, BMO]):
 
     @abstractmethod
-    async def execute(self, params: BMI) -> dict[str, Any] | list[dict[str, Any]]: ...
+    async def execute(self, params: BMI) -> BMO | list[BMO]: ...
