08c199d640d25e899d4e941aee3dcdbb0c7cde60
Gabriel Nobrega <gabrieltkdnobrega63@gmail.com>
2024-03-15 18:29:43 -0300

    fix: weapon effects

--- src/utils/weapon.py
+++ src/utils/weapon.py
@@ -1,4 +1,2 @@
 def weapon_effect_title(text: str) -> str:
-    if text and text.endswith(":"):
-        return text.replace(":", "").replace("*", "")
-    return text
+    return text.replace(":**", "").replace("**", "")
