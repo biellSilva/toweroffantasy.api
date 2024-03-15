24e169e219d31b297a9799018a13b1a629a0be01
Gabriel Nobrega <gabrieltkdnobrega63@gmail.com>
2024-03-15 02:11:24 -0300

    feat: add cn matrices data

--- src/data/usecases/matrices/find.py
+++ src/data/usecases/matrices/find.py
@@ -1,13 +1,19 @@
-from src.domain.errors.http import NotFoundErr, NotImplementedErr, VersionNotFoundErr
+from src.domain.errors.http import NotFoundErr, VersionNotFoundErr
 from src.domain.models.matrices import Matrix
 from src.domain.usecases.base import FindParams
 from src.domain.usecases.matrices.find import IFindMatricesUseCase
+from src.infra.repository.matrices.china import MatricesChinaRepository
 from src.infra.repository.matrices.global_ import MatricesGlobalRepository
 
 
 class FindMatricesUseCase(IFindMatricesUseCase):
-    def __init__(self, repository: MatricesGlobalRepository) -> None:
+    def __init__(
+        self,
+        repository: MatricesGlobalRepository,
+        china_repository: MatricesChinaRepository,
+    ) -> None:
         self.repository = repository
+        self.china_repository = china_repository
 
     async def execute(self, params: FindParams) -> Matrix:
         if params.version == "global":
@@ -16,7 +22,9 @@ class FindMatricesUseCase(IFindMatricesUseCase):
             raise NotFoundErr
 
         elif params.version == "china":
-            raise NotImplementedErr
+            if data := await self.china_repository.find_by_id(**params.model_dump()):
+                return data
+            raise NotFoundErr
 
         else:
             raise VersionNotFoundErr
--- src/data/usecases/matrices/get_all.py
+++ src/data/usecases/matrices/get_all.py
@@ -1,21 +1,27 @@
 from src.decorators.filter import filter_models
-from src.domain.errors.http import NotImplementedErr, VersionNotFoundErr
+from src.domain.errors.http import VersionNotFoundErr
 from src.domain.models.matrices import Matrix
 from src.domain.usecases.base import GetAllParams
 from src.domain.usecases.matrices.get_all import IGetAllMatricesUseCase
+from src.infra.repository.matrices.china import MatricesChinaRepository
 from src.infra.repository.matrices.global_ import MatricesGlobalRepository
 
 
 class GetAllMatricesUseCase(IGetAllMatricesUseCase):
-    def __init__(self, repository: MatricesGlobalRepository) -> None:
+    def __init__(
+        self,
+        repository: MatricesGlobalRepository,
+        china_repository: MatricesChinaRepository,
+    ) -> None:
         self.repository = repository
+        self.china_repository = china_repository
 
     async def execute(self, params: GetAllParams) -> list[Matrix]:
         if params.version == "global":
             models = await self.repository.get_all(**params.model_dump())
 
         elif params.version == "china":
-            raise NotImplementedErr
+            models = await self.china_repository.get_all(**params.model_dump())
 
         else:
             raise VersionNotFoundErr
