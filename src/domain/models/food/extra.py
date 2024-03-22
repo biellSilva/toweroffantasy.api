28d47bbaca249f1394b2a1e05eec2c3704772579
FortOfFans <56215456+FortOfFans@users.noreply.github.com>
2024-03-22 14:50:22 +0100

    [GL] v3.7.3 [CN] v3.7.53

--- src/domain/models/food/extra.py
+++ src/domain/models/food/extra.py
@@ -10,7 +10,7 @@ class Ingredient(BaseModel):
     matID: Item
     min: Annotated[int, BeforeValidator(lambda x: int(x))]
     max: Annotated[int, BeforeValidator(lambda x: int(x))]
-    source: list[str]
+    source: list[]
 
 
 @strawberry.experimental.pydantic.type(model=Ingredient, all_fields=True)
