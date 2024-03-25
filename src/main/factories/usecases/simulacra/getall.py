ef7f50d81929dc86750cfa6285d05ab53560562f
Gabriel Nobrega <gabrieltkdnobrega63@gmail.com>
2024-03-25 12:59:02 -0300

    feat: add simulacra chinese version

--- src/main/factories/usecases/simulacra/getall.py
+++ src/main/factories/usecases/simulacra/getall.py
@@ -1,4 +1,5 @@
 from src.data.usecases.simulacra.getall import GetallSimulacraUseCase
+from src.infra.repository.simulacra.china import SimulacraChinaRepository
 from src.infra.repository.simulacra.global_ import SimulacraGlobalRepository
 
 
@@ -6,4 +7,6 @@ class GetallSimulacraUsecaseFactory:
 
     @staticmethod
     def create() -> GetallSimulacraUseCase:
-        return GetallSimulacraUseCase(SimulacraGlobalRepository())
+        return GetallSimulacraUseCase(
+            SimulacraGlobalRepository(), SimulacraChinaRepository()
+        )
