76e931504ae3084695a3f8fb556b3699ec47ab35
Gabriel Nobrega <gabrieltkdnobrega63@gmail.com>
2024-03-26 01:59:47 -0300

    feat: add scalar docs

--- src/main/app/routes/__init__.py
+++ src/main/app/routes/__init__.py
@@ -1,12 +1,15 @@
 from fastapi import APIRouter
 
+from src.main.app.routes import docs
+
 from . import assets, graphql, simulacra
 
 router = APIRouter(prefix="")
 
-router.include_router(router=graphql.router, tags=["Graphql"])
+router.include_router(router=graphql.router, tags=["Graphql"], include_in_schema=False)
 router.add_websocket_route(path="/graphql", endpoint=graphql.router)  # type: ignore
 
 router.include_router(router=simulacra.router)
 
 router.include_router(router=assets.router)
+router.include_router(router=docs.router)
