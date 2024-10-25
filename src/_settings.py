320b27dfc45dbdce9d0b2cf1599718797eecfcc4
Gabriel Nobrega <gabrieltkdnobrega63@gmail.com>
2024-10-25 19:14:13 -0300

    refactor: update settings module with validation rules
    - Add validation rules for email, password, and username fields in the settings module.
    - Define minimum and maximum lengths for email, password, and username.
    - Specify regular expressions for email, password, and username validation.
    - Update the settings module to include the DB_URL and JWT_SECRET variables.

--- src/_settings.py
+++ src/_settings.py
@@ -12,5 +12,25 @@ class _BaseSettings(BaseSettings):
 
     PROJECT_NAME: str = "Tower of Fantasy API"
 
+    DB_URL: str
 
-config = _BaseSettings()
+    JWT_SECRET: str = "secret"
+
+    EMAIL_MIN_LENGTH: int = 4
+    EMAIL_MAX_LENGTH: int = 256
+    EMAIL_REGEX: str = r"^(?=.{4,256}$)[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+
+    PASSWORD_MIN_LENGTH: int = 8
+    PASSWORD_MAX_LENGTH: int = 256
+    PASSWORD_SPECIAL_CHARS: str = "@$!%*?&"
+    PASSWORD_REGEX: str = (
+        r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,256}$"
+    )
+
+    USERNAME_MIN_LENGTH: int = 4
+    USERNAME_MAX_LENGTH: int = 30
+    USERNAME_SPECIAL_CHARS: str = "_."
+    USERNAME_REGEX: str = r"^[a-zA-Z0-9_\.]{4,30}$"
+
+
+config = _BaseSettings()  # type: ignore[call-arg]
