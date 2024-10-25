9e6480506fda1043f58ebfacb4b20427c784574b
Gabriel Nobrega <gabrieltkdnobrega63@gmail.com>
2024-10-25 19:17:17 -0300

    feat: Add NotFoundError and UserNotFoundError classes
    Add NotFoundError class to handle not found errors and UserNotFoundError class to handle user not found errors. These classes inherit from the base ApiError class and provide custom error messages and status codes.

---
+++ src/exceptions/not_found.py
@@ -0,0 +1,25 @@
+from typing import Any
+
+from src.exceptions.base import ApiError
+
+
+class NotFoundError(ApiError):
+    """Not found error."""
+
+    def __init__(
+        self,
+        message: str = "Resource not found.",
+        **metadata: str | float | dict[str, Any] | list[Any],
+    ) -> None:
+        super().__init__(message=message, status_code=404, **metadata)
+
+
+class UserNotFoundError(NotFoundError):
+    """User not found error."""
+
+    def __init__(
+        self,
+        message: str = "User not found.",
+        **metadata: str | float | dict[str, Any] | list[Any],
+    ) -> None:
+        super().__init__(message=message, **metadata)
