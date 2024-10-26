5541af42d9cd363851c09154e69c3d0f54f11915
Gabriel Nobrega <gabrieltkdnobrega63@gmail.com>
2024-10-25 21:38:05 -0300

    feat: add main router to include user router after auth router

---
+++ src/modules/router.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter
+
+from src.modules.auth.controller import router as auth_router
+from src.modules.user.controller import router as user_router
+
+router = APIRouter()
+
+router.include_router(auth_router)
+# Auth router is included first
+
+router.include_router(user_router)
