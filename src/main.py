0dd453bb34f05fa3d54d233a35e793f65e6b4cb4
Gabriel Nobrega <gabrieltkdnobrega63@gmail.com>
2024-10-25 21:38:38 -0300

    refactor: Add function to include user router in main app
    This commit refactors the `create_app` function in `main.py` to include the user router in the main FastAPI app. This allows the user router to be accessed after the authentication router. The `add_routes` function is introduced to handle the inclusion of the user router. This change improves the organization and modularity of the codebase.

--- src/main.py
+++ src/main.py
@@ -2,11 +2,22 @@ from fastapi import FastAPI
 
 from src._lifespan import lifespan
 from src._settings import config
+from src.exceptions.base import ApiError
 
 
 def create_app() -> FastAPI:
-    return FastAPI(
+    app = FastAPI(
         title=config.PROJECT_NAME,
         lifespan=lifespan,
         exception_handlers={ApiError: ApiError.handle_error},
     )
+
+    add_routes(app)
+
+    return app
+
+
+def add_routes(app: FastAPI) -> None:
+    from src.modules.router import router
+
+    app.include_router(router)
