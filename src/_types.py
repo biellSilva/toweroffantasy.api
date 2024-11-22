c619bb76ad40630770f48059ba3447764d531d55
Gabriel Nobrega <gabrieltkdnobrega63@gmail.com>
2024-11-22 13:32:15 -0300

    feat: add LangsEnum for supported languages

---
+++ src/_types.py
@@ -0,0 +1,14 @@
+from enum import StrEnum
+
+
+class LangsEnum(StrEnum):
+    DE = "de"
+    EN = "en"
+    ES = "es"
+    FR = "fr"
+    ID = "id"
+    JA = "ja"
+    PT = "pt"
+    RU = "ru"
+    TH = "th"
+    ZH_CN = "zh-CN"
