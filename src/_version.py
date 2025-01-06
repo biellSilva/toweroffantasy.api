16ec7bafeeaee26bb433a75564c6233c03a895a1
Gabriel Nobrega <gabrieltkdnobrega63@gmail.com>
2025-01-06 00:13:00 -0300

    feat: update version handling to return default if VERSION file is missing

--- src/_version.py
+++ src/_version.py
@@ -1,4 +1,9 @@
 def get_version() -> str:
     from pathlib import Path
 
-    return Path("src/VERSION.txt").read_text(encoding="utf-8").strip()
+    version_file = Path(__file__).parent / "VERSION"
+
+    if not version_file.is_file():
+        return "0.0.0"
+
+    return version_file.read_text(encoding="utf-8").strip()
