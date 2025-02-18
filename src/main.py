089e6d8576bace7fe38e8bb5c22af40611670052
Gabriel Nobrega <gabrieltkdnobrega63@gmail.com>
2025-02-18 11:01:21 -0300

    feat: add project summary and version to FastAPI app initialization

--- src/main.py
+++ src/main.py
@@ -2,14 +2,17 @@ from fastapi import FastAPI
 
 from src._lifespan import lifespan
 from src._settings import config
+from src._version import get_version
 from src.exceptions.base import ApiError
 
 
 def create_app() -> FastAPI:
     app = FastAPI(
         title=config.PROJECT_NAME,
+        summary=config.project_summary,
         lifespan=lifespan,
         exception_handlers={ApiError: ApiError.handle_error},
+        version=get_version(),
     )
 
     add_routes(app)
