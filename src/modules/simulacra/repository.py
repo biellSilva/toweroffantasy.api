678b01ecc5bd98b24539485a1737f3b1e2535589
Gabriel Nobrega <gabrieltkdnobrega63@gmail.com>
2025-02-21 09:40:33 -0300

    feat: refactor repository classes to inherit from JsonRepository

--- src/modules/simulacra/repository.py
+++ src/modules/simulacra/repository.py
@@ -1,40 +1,6 @@
-from json import loads
-from typing import Any
-
-import aiofiles
-
-from src._types import LangsEnum
-from src.modules.base.cache import RedisCache
+from src.modules.base.json_repository import JsonRepository
 
 
-class ImitationRepository:
-    _NAME = "simulacra"
-
+class ImitationRepository(JsonRepository):
     def __init__(self) -> None:
-        self._cache = RedisCache(name=self._NAME)
-
-    async def _loads_json(self, *, lang: LangsEnum) -> None:
-        """Load all items from the database."""
-
-        async with aiofiles.open(f"src/database/{lang}/{self._NAME}.json") as file:
-            data = loads(await file.read())
-
-        await self._cache.set_values(lang=lang, value_dict=data)
-
-    async def get_id(self, *, lang: LangsEnum, _id: str) -> dict[str, Any] | None:
-        """Get a item by id."""
-        cache = await self._cache.get(lang=lang, key=_id)
-
-        if not cache:
-            await self._loads_json(lang=lang)
-
-        return await self._cache.get(lang=lang, key=_id)
-
-    async def get_all(self, *, lang: LangsEnum) -> list[dict[str, Any]]:
-        """Get all items."""
-        cache = await self._cache.get_all(lang=lang)
-
-        if not cache:
-            await self._loads_json(lang=lang)
-
-        return list((await self._cache.get_all(lang=lang)).values())
+        super().__init__(name="simulacra")
