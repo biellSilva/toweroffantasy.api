f24d524f9bd3c1d9ad5b765305ac962ac2763d15
Gabriel Nobrega <gabrieltkdnobrega63@gmail.com>
2025-02-22 11:26:34 -0300

    feat: add pagination and string comparison utilities to enhance data handling

--- src/_utils.py
+++ src/_utils.py
@@ -1,6 +1,32 @@
 from datetime import datetime
 from zoneinfo import ZoneInfo
 
+from pydantic import BaseModel
+from unidecode import unidecode
+
 
 def current_datetime() -> datetime:
     return datetime.now(ZoneInfo("America/Sao_Paulo"))
+
+
+def paginate_items[T: BaseModel](
+    data: list[T],
+    page: int,
+    limit: int,
+) -> list[T]:
+    start = (page - 1) * limit
+    return data[start : start + limit]
+
+
+def is_str_in_list(
+    param: str,
+    list_: list[str],
+    *,
+    equals: bool = False,
+) -> bool:
+    if equals:
+        return any(
+            unidecode(param).lower() == unidecode(item).lower() for item in list_
+        )
+
+    return any(unidecode(param).lower() in unidecode(item).lower() for item in list_)
