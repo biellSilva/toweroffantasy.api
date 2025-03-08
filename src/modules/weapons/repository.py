f48d3206e85fde6dd7e7b0848c6f3686d8a4540e
Gabriel Nobrega <gabrieltkdnobrega63@gmail.com>
2025-03-08 03:17:52 -0300

    feat: add sorting functionality to get_all method in WeaponRepository

--- src/modules/weapons/repository.py
+++ src/modules/weapons/repository.py
@@ -1,3 +1,4 @@
+from src.modules._utils import rarity_to_int
 from src.modules.base.json_repository import JsonRepository
 from src.modules.weapons.model import Weapon, WeaponSimple
 
@@ -5,3 +6,9 @@ from src.modules.weapons.model import Weapon, WeaponSimple
 class WeaponRepository(JsonRepository[Weapon, WeaponSimple]):
     def __init__(self) -> None:
         super().__init__(name="weapons", model=Weapon, simple_model=WeaponSimple)
+
+    async def get_all(self, *, lang: str) -> list[WeaponSimple]:
+        return sorted(
+            await super().get_all(lang=lang),
+            key=lambda x: (-x.is_limited, -rarity_to_int(x.rarity), x.name),
+        )
