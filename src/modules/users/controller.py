1007f7543d0e6be056a24716e866976e17c81d1d
Gabriel Nobrega <gabrieltkdnobrega63@gmail.com>
2025-03-21 00:51:40 -0300

    feat: enhance get_user_me endpoint documentation to include authentication details

--- src/modules/users/controller.py
+++ src/modules/users/controller.py
@@ -2,6 +2,7 @@ from typing import Annotated
 
 from fastapi import APIRouter, Path, Security
 
+from src.core.docs import generate_docs
 from src.modules.auth.dtos import Payload
 from src.modules.users.dtos import User, UserMe
 from src.modules.users.repository import UserRepository
@@ -13,14 +14,12 @@ router = APIRouter(prefix="/users", tags=["users"])
 SERVICE = UserService(UserRepository())
 
 
-@router.get(path="/@me")
+@router.get(path="/@me", responses=generate_docs(auth=True))
 async def get_user_me(user: Annotated[Payload, Security(AuthSecurity())]) -> UserMe:
     return await SERVICE.get_user_me(user_id=user.id)
 
 
 @router.get(path="/{user_id}")
-async def get_user_by_id(
-    user_id: Annotated[int, Path()],
-) -> User:
+async def get_user_by_id(user_id: Annotated[int, Path()]) -> User:
     """Get a user by ID."""
     return await SERVICE.get_user_by_id(user_id)
