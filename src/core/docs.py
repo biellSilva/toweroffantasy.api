2b467e48a4ab99265344a457820f8c6fbb876ba9
Gabriel Nobrega <gabrieltkdnobrega63@gmail.com>
2025-03-21 00:48:40 -0300

    feat: enhance documentation generation for exceptions with authentication flags

--- src/core/docs.py
+++ src/core/docs.py
@@ -1,15 +1,28 @@
 from typing import Any
 
 from src.exceptions.base import ApiError
+from src.exceptions.unauthorized import (
+    ExpiredTokenError,
+    InvalidTokenError,
+    MissingTokenError,
+)
 
 
-def generate_docs(*excs: type[ApiError] | ApiError) -> dict[Any, Any]:
+def generate_docs(
+    *excs: type[ApiError] | ApiError,
+    auth: bool = False,
+) -> dict[Any, Any]:
     """Generate docs for exception.
 
     Args:
         excs (type[ApiError] | ApiError): Exception class or instance.
+        auth (bool): Authentication flags.
     """
     docs: dict[Any, Any] = {}
+
+    if auth:
+        excs = (*excs, MissingTokenError, ExpiredTokenError, InvalidTokenError)
+
     for exc in excs:
         if not isinstance(exc, ApiError):
             exc = exc()  # noqa: PLW2901
--- src/modules/banners/controller.py
+++ src/modules/banners/controller.py
@@ -3,6 +3,8 @@ from typing import Annotated
 from fastapi import APIRouter, Body, Query, Security
 from prisma.models import Banner
 
+from src.core.docs import generate_docs
+from src.exceptions.unauthorized import InvalidRoleError
 from src.modules.auth.dtos import Payload
 from src.modules.banners.dtos import CreateBanner, GetBanners
 from src.modules.banners.repository import BannerRepository
@@ -19,7 +21,7 @@ async def get_versions(params: Annotated[GetBanners, Query()]) -> list[Banner]:
     return await SERVICE.get_banners(params)
 
 
-@router.post("")
+@router.post("", responses=generate_docs(InvalidRoleError))
 async def create_version(
     _: Annotated[Payload, Security(RoleSecurity(role="admin"))],
     params: Annotated[CreateBanner, Body()],
