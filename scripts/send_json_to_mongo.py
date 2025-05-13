2e59f785e5d810b76b68ef4fc232914f043c07b2
Gabriel Nobrega <gabrieltkdnobrega63@gmail.com>
2025-05-13 12:00:06 -0300

    refactor: remove obsolete scripts and implement relations fixing in send_json_to_mongo.py

--- scripts/send_json_to_mongo.py
+++ scripts/send_json_to_mongo.py
@@ -7,7 +7,7 @@ from pathlib import Path
 from typing import Any
 
 import dotenv
-from motor.motor_asyncio import AsyncIOMotorClient
+from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
 
 
 def load_data(path: str | Path) -> dict[str, dict[str, Any]]:
@@ -26,9 +26,49 @@ COLLECTIONS = [
 ]
 
 
+async def fix_relations(mongo_db: AsyncIOMotorDatabase) -> None:
+    relations = mongo_db.get_collection("relations")
+    simulacra = load_data("src/database/simulacra.json")
+    weapons = load_data("src/database/weapons.json")
+
+    for simulacrum in simulacra.values():
+        relation: dict[str, Any] = {
+            "imitation_id": simulacrum["id"],
+            "weapon_id": None,
+            "suit_id": None,
+        }
+        for weapon in weapons.values():
+            if (
+                simulacrum["weapon_id"]
+                and weapon["id"].lower() == simulacrum["weapon_id"].lower()
+            ):
+                relation["weapon_id"] = weapon["id"]
+
+                if any(
+                    weapon["id"].lower() == x.lower()
+                    for x in [
+                        "stave_thunder",
+                        "digger_thunder",
+                    ]
+                ):
+                    relation["suit_id"] = weapon["recommended_matrices"][1]["id"]
+
+                else:
+                    relation["suit_id"] = weapon["recommended_matrices"][0]["id"]
+                break
+
+        await relations.find_one_and_update(
+            filter={"imitation_id": simulacrum["id"]},
+            update={"$set": relation},
+            upsert=True,
+        )
+
+        print(f"{simulacrum['id']} sent!")  # noqa: T201
+
+
 async def main() -> None:
     mongo = AsyncIOMotorClient(dotenv.get_key(dotenv.find_dotenv(), "MONGO_URI"))
-    mongo_db = mongo.get_database("dev")
+    mongo_db = mongo.get_database("prod")
 
     for collection in COLLECTIONS:
         _data = load_data(f"src/database/{collection}.json")
@@ -37,7 +77,7 @@ async def main() -> None:
         mongo_collection = mongo_db.get_collection(collection)
 
         for data in _data.values():
-            if collection == "matrices":
+            if collection == "matrices" and str(data["id"]).startswith("S"):
                 data["id"] = str(data["id"]).replace("S", "s", 1)
 
             await mongo_collection.find_one_and_update(
@@ -47,6 +87,8 @@ async def main() -> None:
             )
 
             print(f"{data['id']} sent!")  # noqa: T201
+
+    await fix_relations(mongo_db)
 
     print("Done!")  # noqa: T201
 
